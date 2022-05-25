13c72ceacf0f84c7eae5055f3d1d0fab21a5949a
Champagnest <mchampagned@protonmail.com>
2022-05-24 19:00:43 -0700

    got mostly everything in place, needs pictures

---
+++ src/content_option.js
@@ -0,0 +1,157 @@
+const logotext = "Mari";
+const meta = {
+    title: "Mari Champagne",
+    description: "I’m Mari Champagne a Full stack devloper,currently working remotely out of the greater Bay Area.",
+};
+
+const introdata = {
+    title: "I’m Mari Champagne",
+    animated: {
+        first: "I enjoy coding dynmaic websites and apps",
+        second: "I enjoy the outdoors",
+        third: "I am open to work",
+    },
+    description: "A full stack developer utilizing Javascript and MERN",
+    your_img_url: "",
+};
+
+const dataabout = {
+    title: "About me",
+    aboutme: "I have many interests and talents including but not limited to music and social issues. I am a recent graduate of both SFSU (BA in Women's and gender studies) and UC Berkeley Extension (full stack web dev certificate). When I'm not coding I like to be outside hiking or doing a board sport.",
+};
+const worktimeline = [{
+        jobtitle: "Fedex, Equipment operator",
+        where: "multiple locations",
+        date: "2017-2021",
+    },
+    {
+        jobtitle: "Freelance Developer",
+        where: "Remote",
+        date: "current",
+    },
+];
+
+const skills = [{
+        name: "SQL",
+        value: 80,
+    },
+    {
+        name: "Node",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 90,
+    },
+    {
+        name: "React",
+        value: 80,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "Front End",
+        description: "I have all of your frontend needs covered with HTML CSS and Javascript.",
+    },
+    {
+        title: "Full stack website/app",
+        description: "Your wish (and money) are my command. I can make (almost) anything using MERN.",
+    },
+];
+
+const dataportfolio = [{
+        img: "",
+        desctiption: "A note taking app using Express.js",
+        link: "https://epicxnote.herokuapp.com/",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        desctiption: "A collabrative app used for finding gas near you",
+        link: "https://pump-plunder.herokuapp.com/",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "mchampagned@protonmail.com",
+    // YOUR_FONE: "(555)123-4567",
+    description: "Here is my email, you can send a message directly or by the form below. Thank you and looking forward to hearing from you. ",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/Champagnest",
+    // facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com/in/marielle-champagne/",
+    // twitter: "https://twitter.com",
+    // youtube: "https://youtube.com",
+    // twitch: "https://twitch.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
